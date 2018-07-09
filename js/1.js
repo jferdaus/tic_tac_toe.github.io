673e0999b3881d011047f956c14d33ab5ea3c020
Jannatul Ferdaus <39017624+jferdaus@users.noreply.github.com>
2018-07-09 15:40:21 +0600

    web publish

---
+++ js/1.js
@@ -0,0 +1,819 @@
+
+
+$(document).ready(function(){
+
+$("#p1").hide();
+$("#p2").hide();
+
+$("#btn1").click(function(){
+
+
+if($("#btn1").text() == ""){
+
+        $("#btn1").prepend("x");
+
+
+
+if($("#btn2").text() == ""){
+
+         $("#btn2").prepend("0");
+
+     }
+
+ else if($("#btn3").text() == ""){
+
+         $("#btn3").prepend("0");
+
+     }
+
+
+else if($("#btn4").text() == ""){
+
+         $("#btn4").prepend("0");
+
+     }
+
+else if($("#btn5").text() == ""){
+
+         $("#btn5").prepend("0");
+
+     }
+
+
+
+else if($("#btn6").text() == ""){
+
+         $("#btn6").prepend("0");
+
+     }
+
+
+
+else if($("#btn7").text() == ""){
+
+         $("#btn7").prepend("0");
+
+     }
+
+
+
+else if($("#btn8").text() == ""){
+
+         $("#btn8").prepend("0");
+
+     }
+
+
+else if($("#btn9").text() == ""){
+
+         $("#btn9").prepend("0");
+
+     }
+
+        
+    }
+});
+
+
+
+
+
+
+
+
+$("#btn2").click(function(){
+
+if($("#btn2").text() == ""){
+
+        $("#btn2").prepend("x");
+
+        
+    }
+
+
+
+    if($("#btn1").text() == ""){
+
+         $("#btn1").prepend("0");
+
+     }
+
+
+else if($("#btn3").text() == ""){
+
+         $("#btn3").prepend("0");
+
+     }
+
+	 
+else if($("#btn4").text() == ""){
+
+         $("#btn4").prepend("0");
+
+     }
+
+
+
+else if($("#btn5").text() == ""){
+
+         $("#btn5").prepend("0");
+
+     }
+
+
+
+else if($("#btn6").text() == ""){
+
+         $("#btn6").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn7").text() == ""){
+
+         $("#btn7").prepend("0");
+
+     }
+
+
+
+
+
+
+else if($("#btn8").text() == ""){
+
+         $("#btn8").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn9").text() == ""){
+
+         $("#btn9").prepend("0");
+
+     }
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+$("#btn3").click(function(){
+
+if($("#btn3").text() == ""){
+
+        $("#btn3").prepend("x");
+
+         
+
+
+
+if($("#btn1").text() == ""){
+
+         $("#btn1").prepend("0");
+
+     }
+
+
+
+else if($("#btn2").text() == ""){
+
+         $("#btn2").prepend("0");
+
+     }
+
+
+
+
+else if($("#btn4").text() == ""){
+
+         $("#btn4").prepend("0");
+
+     }
+
+
+
+else if($("#btn5").text() == ""){
+
+         $("#btn5").prepend("0");
+
+     }
+
+
+
+else if($("#btn6").text() == ""){
+
+         $("#btn6").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn7").text() == ""){
+
+         $("#btn7").prepend("0");
+
+     }
+
+
+
+
+
+
+else if($("#btn8").text() == ""){
+
+         $("#btn8").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn9").text() == ""){
+
+         $("#btn9").prepend("0");
+
+     }
+
+   
+    }
+
+});
+
+
+
+//button 4
+$("#btn4").click(function(){
+
+if($("#btn4").text() == ""){
+
+        $("#btn4").prepend("x");
+
+         
+
+
+
+if($("#btn1").text() == ""){
+
+         $("#btn1").prepend("0");
+
+     }
+
+
+
+else if($("#btn2").text() == ""){
+
+         $("#btn2").prepend("0");
+
+     }
+
+
+
+
+else if($("#btn3").text() == ""){
+
+         $("#btn3").prepend("0");
+
+     }
+
+
+
+else if($("#btn5").text() == ""){
+
+         $("#btn5").prepend("0");
+
+     }
+
+
+
+else if($("#btn6").text() == ""){
+
+         $("#btn6").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn7").text() == ""){
+
+         $("#btn7").prepend("0");
+
+     }
+
+
+else if($("#btn8").text() == ""){
+
+         $("#btn8").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn9").text() == ""){
+
+         $("#btn9").prepend("0");
+
+     }
+   
+    }
+
+});
+
+
+
+
+
+//button 5
+
+$("#btn5").click(function(){
+
+if($("#btn5").text() == ""){
+
+        $("#btn5").prepend("x");
+
+         
+
+
+
+if($("#btn1").text() == ""){
+
+         $("#btn1").prepend("0");
+
+     }
+
+
+
+else if($("#btn2").text() == ""){
+
+         $("#btn2").prepend("0");
+
+     }
+
+
+
+
+else if($("#btn3").text() == ""){
+
+         $("#btn3").prepend("0");
+
+     }
+
+
+
+else if($("#btn4").text() == ""){
+
+         $("#btn4").prepend("0");
+
+     }
+
+
+
+else if($("#btn6").text() == ""){
+
+         $("#btn6").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn7").text() == ""){
+
+         $("#btn7").prepend("0");
+
+     }
+
+
+
+
+
+
+else if($("#btn8").text() == ""){
+
+         $("#btn8").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn9").text() == ""){
+
+         $("#btn9").prepend("0");
+
+     }
+   
+    }
+
+});
+
+
+//button 6
+$("#btn6").click(function(){
+
+if($("#btn6").text() == ""){
+
+        $("#btn6").prepend("x");
+
+       
+
+
+
+if($("#btn1").text() == ""){
+
+         $("#btn1").prepend("0");
+
+     }
+
+
+
+else if($("#btn2").text() == ""){
+
+         $("#btn2").prepend("0");
+
+     }
+
+
+
+else if($("#btn3").text() == ""){
+
+         $("#btn3").prepend("0");
+
+     }
+
+
+
+
+else if($("#btn4").text() == ""){
+
+         $("#btn4").prepend("0");
+
+     }
+
+
+
+else if($("#btn5").text() == ""){
+
+         $("#btn5").prepend("0");
+
+     }
+
+
+
+else if($("#btn7").text() == ""){
+
+         $("#btn7").prepend("0");
+
+     }
+
+
+else if($("#btn8").text() == ""){
+
+         $("#btn8").prepend("0");
+
+     }
+
+
+else if($("#btn9").text() == ""){
+
+         $("#btn9").prepend("0");
+
+     }
+   
+    }
+
+});
+
+
+
+
+
+//button 7
+$("#btn7").click(function(){
+
+if($("#btn7").text() == ""){
+
+        $("#btn7").prepend("x");
+
+         
+
+
+ if($("#btn1").text() == ""){
+
+         $("#btn1").prepend("0");
+
+     }
+
+
+else if($("#btn2").text() == ""){
+
+         $("#btn2").prepend("0");
+
+     }
+
+
+
+else if($("#btn3").text() == ""){
+
+         $("#btn3").prepend("0");
+
+     }
+
+
+
+
+else if($("#btn4").text() == ""){
+
+         $("#btn4").prepend("0");
+
+     }
+
+
+
+else if($("#btn5").text() == ""){
+
+         $("#btn5").prepend("0");
+
+     }
+
+	 
+	 else if($("#btn6").text() == ""){
+
+         $("#btn6").prepend("0");
+
+     }
+
+else if($("#btn8").text() == ""){
+
+         $("#btn8").prepend("0");
+
+     }
+
+
+else if($("#btn9").text() == ""){
+
+         $("#btn9").prepend("0");
+
+     }
+
+    }
+
+});
+
+
+
+
+$("#btn8").click(function(){
+
+if($("#btn8").text() == ""){
+
+        $("#btn8").prepend("x");
+
+
+if($("#btn1").text() == ""){
+
+         $("#btn1").prepend("0");
+
+     }
+
+
+
+else if($("#btn2").text() == ""){
+
+         $("#btn2").prepend("0");
+
+     }
+
+
+
+else if($("#btn3").text() == ""){
+
+         $("#btn3").prepend("0");
+
+     }
+
+
+
+
+else if($("#btn4").text() == ""){
+
+         $("#btn4").prepend("0");
+
+     }
+
+
+
+else if($("#btn5").text() == ""){
+
+         $("#btn5").prepend("0");
+
+     }
+
+
+
+
+else if($("#btn6").text() == ""){
+
+         $("#btn6").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn7").text() == ""){
+
+         $("#btn7").prepend("0");
+
+     }
+
+
+
+
+else if($("#btn9").text() == ""){
+
+         $("#btn9").prepend("0");
+
+     }
+
+    }
+
+});
+
+
+
+$("#btn9").click(function(){
+
+
+if($("#btn9").text() == ""){
+
+        $("#btn9").prepend("x");
+
+
+
+    if($("#btn1").text() == ""){
+
+        $("#btn1").prepend("0");
+
+     }
+
+
+
+
+   else if($("#btn2").text() == ""){
+
+         $("#btn2").prepend("0");
+
+     }
+
+
+
+ else if($("#btn3").text() == ""){
+
+         $("#btn3").prepend("0");
+
+     }
+
+
+else if($("#btn4").text() == ""){
+
+         $("#btn4").prepend("0");
+
+     }
+
+
+
+else if($("#btn5").text() == ""){
+
+         $("#btn5").prepend("0");
+
+     }
+
+
+
+else if($("#btn6").text() == ""){
+
+         $("#btn6").prepend("0");
+
+     }
+
+
+
+
+
+else if($("#btn7").text() == ""){
+
+         $("#btn7").prepend("0");
+
+     }
+
+
+
+
+
+
+else if($("#btn8").text() == ""){
+
+         $("#btn8").prepend("0");
+
+     }
+     
+   
+    }
+
+});
+
+
+
+  $(".game").mouseenter(function(){
+       
+
+if($("#btn2").text() == "x" && $("#btn5").text() == "x" && $("#btn8").text()=="x"){  $("#p1").show() ;}
+
+else if($("#btn1").text() == "x" && $("#btn4").text() == "x" && $("#btn7").text()=="x"){ $("#p1").show() ; }
+
+else if($("#btn3").text() == "x" && $("#btn6").text() == "x" && $("#btn9").text()=="x"){ $("#p1").show() ; }
+
+
+
+else if($("#btn1").text() == "x" && $("#btn2").text() == "x" && $("#btn3").text()=="x"){ $("#p1").show() ; }
+else if($("#btn4").text() == "x" && $("#btn5").text() == "x" && $("#btn6").text()=="x"){ $("#p1").show() ; }
+else if($("#btn7").text() == "x" && $("#btn8").text() == "x" && $("#btn9").text()=="x"){ $("#p1").show() ; }
+
+
+else if($("#btn1").text() == "x" && $("#btn5").text() == "x" && $("#btn9").text()=="x"){ $("#p1").show() ; }
+else if($("#btn3").text() == "x" && $("#btn5").text() == "x" && $("#btn7").text()=="x"){ $("#p1").show() ; }
+
+   
+   
+   
+   
+else if($("#btn2").text() == "0" && $("#btn5").text() == "0" && $("#btn8").text()=="0"){  $("#p2").show() ;}
+else if($("#btn1").text() == "0" && $("#btn4").text() == "0" && $("#btn7").text()=="0"){ $("#p2").show() ; }
+else if($("#btn3").text() == "0" && $("#btn6").text() == "0" && $("#btn9").text()=="0"){ $("#p2").show() ; }
+
+
+
+else if($("#btn1").text() == "0" && $("#btn2").text() == "0" && $("#btn3").text()=="0"){ $("#p2").show() ; }
+else if($("#btn4").text() == "0" && $("#btn5").text() == "0" && $("#btn6").text()=="0"){ $("#p2").show() ; }
+else if($("#btn7").text() == "0" && $("#btn8").text() == "0" && $("#btn9").text()=="0"){ $("#p2").show() ; }
+
+
+else if($("#btn1").text() == "0" && $("#btn5").text() == "0" && $("#btn9").text()=="0"){ $("#p2").show() ; }
+else if($("#btn3").text() == "0" && $("#btn5").text() == "0" && $("#btn7").text()=="0"){ $("#p2").show() ; }
+
+   
+
+	
+	
+
+	
+
+});
+
+
+
+
+$("#btn11").click(function(){
+
+
+location.reload();
+
+});
+
+
+
+});
+
+
+
+
+
+
+
+
+
